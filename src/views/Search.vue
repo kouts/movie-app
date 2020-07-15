d230695e0b6b88aa909e21f14b6b401b7efecd32
E6440\Kouts <koutsaftakis@gmail.com>
2020-07-15 09:48:31 +0300

    Fixed debounced input handler

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,13 +2,14 @@
   <div class="container">
     <h1 class="mb-4">Search a movie</h1>
     <div class="mb-4">
-      <input type="text" v-model="search" ref="search" class="form-control" @input="search = $event.target.value" placeholder="Enter a movie title to search..." />
+      <input type="text" :value="search" ref="search" class="form-control" @input="searchHandler($event)" placeholder="Enter a movie title to search..." />
     </div>
     <movies-list mode="search" :query="search" />
   </div>
 </template>
 
 <script>
+import { debounce } from 'lodash-es';
 import MoviesList from '@/components/MoviesList.vue';
 
 export default {
@@ -19,6 +20,11 @@ export default {
     return {
       search: ''
     };
+  },
+  created() {
+    this.searchHandler = debounce((event) => {
+      this.search = event.target.value;
+    }, 100);
   },
   mounted() {
     this.$refs.search.focus();
