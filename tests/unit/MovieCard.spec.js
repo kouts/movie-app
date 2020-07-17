aa1bcea28f3040644297ac7e74e12050932a757d
kouts <koutsaftakis@gmail.com>
2020-07-17 13:33:04 +0300

    Added emit test

--- tests/unit/MovieCard.spec.js
+++ tests/unit/MovieCard.spec.js
@@ -4,19 +4,25 @@ import { movies } from './data/movies';
 
 describe('MovieCard.vue', () => {
   const movie = movies.slice(0, 1);
+  const wrapper = shallowMount(MovieCard, {
+    propsData: {
+      movieId: 1,
+      posterPath: movie.poster_path,
+      title: movie.title,
+      releaseDate: movie.release_date,
+      genres: movie.genres,
+      voteAverage: movie.vote_average,
+      overview: movie.overview
+    }
+  });
   it('renders correctly when passed props', () => {
-    const wrapper = shallowMount(MovieCard, {
-      propsData: {
-        movieId: movie.id,
-        posterPath: movie.poster_path,
-        title: movie.title,
-        releaseDate: movie.release_date,
-        genres: movie.genres,
-        voteAverage: movie.vote_average,
-        overview: movie.overview
-      }
-    });
     const card = wrapper.find('div.card');
     expect(card.exists()).toBe(true);
+  });
+
+  it('emits view details event with movieId as payload', async() => {
+    await wrapper.find('h5 > a').trigger('click');
+    expect(wrapper.emitted('view-details')).toBeTruthy();
+    expect(wrapper.emitted('view-details')[0]).toEqual([1]);
   });
 });
