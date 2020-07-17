4060b39e58ca60a3d2e0bf571cf61644f13820c7
kouts <koutsaftakis@gmail.com>
2020-07-17 18:04:19 +0300

    Removed Vue import

--- tests/unit/MovieGenres.spec.js
+++ tests/unit/MovieGenres.spec.js
@@ -1,6 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
 import MovieGenres from '@/components/MovieGenres.vue';
-import Vue from 'vue';
 
 describe('MovieGenres.vue', () => {
   const genres = ['Action', 'Drama', 'History', 'War'];
@@ -15,7 +14,7 @@ describe('MovieGenres.vue', () => {
   });
   it('renders a message when no genres are passed', async() => {
     wrapper.setProps({ genres: [] });
-    await Vue.nextTick();
+    await wrapper.vm.$nextTick();
     const badges = wrapper.findAll('.badge');
     expect(badges.exists()).toBe(false);
     expect(wrapper.find('span').text()).toBe('Unknown genre');
