12b39728fcb150564daeca80d066c92d01c0a9d4
E6440\Kouts <koutsaftakis@gmail.com>
2020-07-28 20:58:43 +0300

    Added site title

--- vue.config.js
+++ vue.config.js
@@ -15,5 +15,11 @@ module.exports = {
         `
       }
     }
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'MovieApp';
+      return args;
+    });
   }
 };
