d97d564d098f23438f2cbdc3cdb663bff682e47d
E6440\Kouts <koutsaftakis@gmail.com>
2020-08-07 00:58:16 +0300

    Removed unneeded config

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [path.resolve(__dirname, './src/scss/styles.scss')]
-    },
-    webpackBundleAnalyzer: {
-      openAnalyzer: false
-    }
-  },
   css: {
     loaderOptions: {
       scss: {
